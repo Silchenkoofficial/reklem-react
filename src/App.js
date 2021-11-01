f8a10ebb87372b291a7e8e420aa376789b6d4dbc
silchenko <nikita.silchenko.work@yandex.ru>
2021-11-01 20:41:33 +0300

    Website Placeholder

--- src/App.js
+++ src/App.js
@@ -12,7 +12,35 @@ const App = () => {
   }, [changedUrl]);
   return (
     <ChakraProvider>
-      <Box m={4}>
+      <Box p={10} height="100vh">
+        <Text
+          display="flex"
+          alignItems="center"
+          fontWeight="extrabold"
+          fontSize="28px"
+          color="#ffffff"
+          fontFamily="Nunito"
+        >
+          Скоро здесь будет самый лучший сайт
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            style={{ marginLeft: 10 }}
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </Text>
+      </Box>
+      {/* <Box m={4}>
         <Text>Вставьте ссылку на видео</Text>
         <form>
           <Input
@@ -44,7 +72,7 @@ const App = () => {
             allowfullscreen
           ></iframe>
         )}
-      </Box>
+      </Box> */}
     </ChakraProvider>
   );
 };
