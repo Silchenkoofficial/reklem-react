b4490124c0a9550841895ea37ab5319b71dbc213
silchenko <nikita.silchenko.work@yandex.ru>
2021-11-01 20:52:38 +0300

    Deleted smile emoji and changed font size of main title text

--- src/App.js
+++ src/App.js
@@ -17,27 +17,12 @@ const App = () => {
           display="flex"
           alignItems="center"
           fontWeight="extrabold"
-          fontSize="28px"
+          fontSize="16px"
+          lineHeight="100%"
           color="#ffffff"
           fontFamily="Nunito"
         >
           Скоро здесь будет самый лучший сайт
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            style={{ marginLeft: 10 }}
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
         </Text>
       </Box>
       {/* <Box m={4}>
